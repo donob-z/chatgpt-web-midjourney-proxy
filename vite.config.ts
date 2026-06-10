e0c391674080142905b257adfa67a01107f1f84e
Don <hxm0070@gmail.com>
2026-06-11 04:29:21 +0800

    域名校验

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig((env) => {
     plugins: setupPlugins(viteEnv),
     server: {
       host: '0.0.0.0',
+      allowedHosts: true,
       port: 1002,
       open: false,
       proxy: {
